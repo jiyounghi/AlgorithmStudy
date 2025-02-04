6ab3ea6495d1ffe8811f63578299d965aa9e167d
jiyounghi <wldud020101@naver.com>
2025-02-04 23:21:25 +0900

    코드 수정

--- 2309)일곱난쟁이.py
+++ 2309)일곱난쟁이.py
@@ -1,32 +1,27 @@
 a = []
-rmv = []
 
 # 아홉개 입력받고 리스트에 저장 
 for i in range(9):
-    b = input()
-    a.append(b)
+    a.append(int(input()))
 
 # 지금 난쟁이들 키의 합 - 원래 난쟁이들 키의 합 100해서 변수에 저장
-a = list(map(int, a))
 total = sum(a) - 100 
 
 # x+y=total을 이용해서 숨어들어온 난쟁이 추려내기
-for x in a:
-    for y in a:
-        if x+y==total and x != y:
-            rmv.append(x)
-            rmv.append(y)
+for i in range(9):
+    for j in range(i+1, 9):
+        if a[i] + a[j] == total:
+            fake1, fake2 = a[i], a[j]
+            found = True
+    else:
+        continue
+    break
 
-# 거짓 난쟁이들 후보 중복제거             
-rmv_set = set(rmv)
-rmv = list(rmv_set)
-
-# 거짓 난쟁이 제거 
-for i in rmv: 
-    a.remove(i)
-a.sort()
+a.remove(fake1)
+a.remove(fake2)
 
 # 진짜 난쟁이들 출력 
+a.sort()
 for i in a:
     print(i)
 
